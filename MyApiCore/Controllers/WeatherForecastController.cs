aeed9e312ef4163add595de7d276d71641bb21c1
Felipe Ercolin <fe_constantine@hotmail.com>
2019-11-25 19:56:17 -0300

    Initial commit

---
+++ MyApiCore/Controllers/WeatherForecastController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyApiCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<WeatherForecast>> Get()
+        {
+            var rng = new Random();
+
+            //Teste
+           // if (1 != 1) return BadRequest();
+
+            return Ok(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            var rng = new Random();
+
+            //Teste
+            //if (1 != 1) return BadRequest();
+
+            return Ok(Enumerable.Range(1, 5)
+                      .Select(index => new WeatherForecast
+                      {
+                          Date = DateTime.Now.AddDays(index),
+                          TemperatureC = rng.Next(-20, 55),
+                          Summary = Summaries[rng.Next(Summaries.Length)]
+                      }).ToArray());
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> GetResult()
+        {
+            var rng = new Random();
+
+            //Teste 
+           // if (1 != 1) return null;
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray();
+        }
+
+        [HttpPost] 
+        [ProducesResponseType(typeof(Product), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult Post(Product product)
+        {
+            if (product.Id == 0) return BadRequest();
+
+            //return Ok(product);//Retorna 200
+            //return CreatedAtAction("Post", product);retorna 201
+            return CreatedAtAction(nameof(Post), product);//retorna 201
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+    }
+}
---
+++ MyApiCore/Model/Fornecedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyApiCore.Model
+{
+    public class Fornecedor
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres.")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [StringLength(14, MinimumLength = 11, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres.")]
+        public string Documento { get; set; }
+        public int TipoFornecedor { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
